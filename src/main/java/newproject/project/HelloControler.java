99e306316a983cf85e6d677266b41ada878d5e10
Jay <jayaraj0964@gmail.com>
2025-03-27 05:51:48 +0530

    first commit

---
+++ src/main/java/newproject/project/HelloControler.java
@@ -0,0 +1,17 @@
+package newproject.project;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+public class HelloControler {
+    
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello coder";
+    }
+    
+
+}
